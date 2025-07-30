18a51e4d1ce7d3d01e9ac4d197da80e4e2be2385
suat kıvam <suatkivam@gmail.com>
2025-07-30 19:06:17 +0300

    added push function

---
+++ utils/push.c
@@ -0,0 +1,8 @@
+#include <utils.h>
+
+void push(t_stack *s_data, int value)
+{
+    if (s_data->top >= s_data->capacity - 1)
+        exit_with_error();
+    s_data->data[++(s_data->top)] = value;
+}
