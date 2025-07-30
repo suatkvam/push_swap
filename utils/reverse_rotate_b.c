77d89f907babae6acd583cf29c414bd13803b01b
suat kıvam <suatkivam@gmail.com>
2025-07-30 21:40:08 +0300

    rrb function added

---
+++ utils/reverse_rotate_b.c
@@ -0,0 +1,17 @@
+#include "utils.h"
+void rrb(t_stack *s_data_b)
+{
+    int i;
+    int last_element;
+
+     if (s_data_b->top < 1)
+        return;
+    last_element = s_data_b->data[0];
+    i = 0;
+    while (i < s_data_b->top)
+    {
+        s_data_b->data[i] = s_data_b->data[i + 1];
+        i++;
+    }
+    s_data_b->data[s_data_b->top] = last_element;
+}
