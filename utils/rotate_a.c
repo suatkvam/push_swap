4317fe9303308653a259e4558e97a599348c310a
suat kıvam <suatkivam@gmail.com>
2025-07-30 21:23:38 +0300

    ra and rb funcitons updated

--- utils/rotate_a.c
+++ utils/rotate_a.c
@@ -1,15 +1,17 @@
 #include "utils.h"
 void ra(t_stack *s_data_a)
 {
-    int fist_element;
+    int i;
+    int first_element;
 
      if (s_data_a->top < 1)
         return;
-    fist_element = s_data_a->data[s_data_a->top];
-    while (s_data_a->top > 0)
+    first_element = s_data_a->data[s_data_a->top];
+    i = s_data_a->top;
+    while (i > 0)
     {
-        s_data_a->data[s_data_a->top] = s_data_a->data[s_data_a->top - 1];
-        s_data_a->top--;
+        s_data_a->data[i] = s_data_a->data[i - 1];
+        i--;
     }
-    s_data_a->data[0] = fist_element;
+    s_data_a->data[0] = first_element;
 }
