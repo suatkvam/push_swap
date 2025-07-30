1810d76f296c5481251642f08b0515960f0aa3cb
suat kıvam <suatkivam@gmail.com>
2025-07-30 20:38:55 +0300

    added pop function

--- utils/utils.h
+++ utils/utils.h
@@ -12,20 +12,21 @@ typedef struct s_stack
     int capacity;
 }   t_stack;
 
-void	*ft_calloc(size_t nmemb, size_t size);
-void init_stack(t_stack *s_data,int capacity);
-void push(t_stack *s_data, int value);
-void free_stack(t_stack *s_data);
-void sa(t_stack *s_data_a);
-void sb(t_stack *s_data_b);
-void ss(t_stack *s_data_a, t_stack *s_data_b);
-void pa(t_stack *s_data_b, t_stack *s_data_a);
-void pb(t_stack *s_data_a, t_stack *s_data_b);
-void ra(t_stack *s_data_a);
-void rb(t_stack *s_data_b);
-void rr(t_stack *s_data_a, t_stack *s_data_b);
-void rra(t_stack *s_data_a);
-void rrb(t_stack *s_data_b);
-void rrr(t_stack *s_data_a, t_stack *s_data_b);
+void    *ft_calloc(size_t nmemb, size_t size);
+void    init_stack(t_stack *s_data,int capacity);
+void    push(t_stack *s_data, int value);
+int     pop(t_stack *s_data);
+void    free_stack(t_stack *s_data);
+void    sa(t_stack *s_data_a);
+void    sb(t_stack *s_data_b);
+void    ss(t_stack *s_data_a, t_stack *s_data_b);
+void    pa(t_stack *s_data_b, t_stack *s_data_a);
+void    pb(t_stack *s_data_a, t_stack *s_data_b);
+void    ra(t_stack *s_data_a);
+void    rb(t_stack *s_data_b);
+void    rr(t_stack *s_data_a, t_stack *s_data_b);
+void    rra(t_stack *s_data_a);
+void    rrb(t_stack *s_data_b);
+void    rrr(t_stack *s_data_a, t_stack *s_data_b);
 
 #endif
