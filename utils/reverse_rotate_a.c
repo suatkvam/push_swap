ab92aa2c528bf2e0f2b8c8a362adcb3da76d8c32
suat kıvam <suatkivam@gmail.com>
2025-07-30 21:39:56 +0300

    rra function added

---
+++ utils/reverse_rotate_a.c
@@ -0,0 +1,17 @@
+#include "utils.h"
+void rra(t_stack *s_data_a)
+{
+    int i;
+    int last_element;
+
+     if (s_data_a->top < 1)
+        return;
+    last_element = s_data_a->data[0];
+    i = 0;
+    while (i < s_data_a->top)
+    {
+        s_data_a->data[i] = s_data_a->data[i + 1];
+        i++;
+    }
+    s_data_a->data[s_data_a->top] = last_element;
+}
