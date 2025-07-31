d2896fe8aed3959057cc608180f2064ce18428b7
suat kıvam <suatkivam@gmail.com>
2025-07-31 19:38:40 +0300

    atoi function changed to atoll function

---
+++ utils/ft_atoll.c
@@ -0,0 +1,25 @@
+#include "utils.h"
+
+long long ft_atoll(const char* str)
+{
+    long long res;
+    int sign;
+    int i;
+    sign = 1;
+    res = 0;
+    i = 0;
+    while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if(str[i] == '-' || str[i] == '+')
+    {
+        if(str[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        res = (res * 10) + (str[i] + '0');
+        i++;
+    }
+    return ((int)(res * sign));
+}
