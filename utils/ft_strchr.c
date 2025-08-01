2ad1e6f4fb4c4e27639b847e0afca2af6720c5e9
suat kıvam <suatkivam@gmail.com>
2025-08-01 13:30:32 +0300

    added ft_strchr function

---
+++ utils/ft_strchr.c
@@ -0,0 +1,18 @@
+#include"utils.h"
+char	*ft_strchr(const char *s, int c)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)s;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((unsigned char)(str[i]) == (unsigned char)c)
+			return (str + i);
+		i++;
+	}
+	if ((unsigned char)c == '\0')
+		return (str + i);
+	return (NULL);
+}
