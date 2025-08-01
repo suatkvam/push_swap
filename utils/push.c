fd933d209b29a265435481883f282f1161ded83f
suat kıvam <suatkivam@gmail.com>
2025-08-01 13:34:32 +0300

    fixed push.c

--- utils/push.c
+++ utils/push.c
@@ -6,11 +6,11 @@
 /*   By: akivam <akivam@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 19:32:41 by akivam            #+#    #+#             */
-/*   Updated: 2025/07/31 20:54:27 by akivam           ###   ########.fr       */
+/*   Updated: 2025/08/01 13:23:08 by akivam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <utils.h>
+#include "utils.h"
 
 void push(t_stack *s_data, int value)
 {
