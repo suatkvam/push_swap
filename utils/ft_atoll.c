09efb4a630e89f3b595795615ed04d0973ab3487
suat kıvam <suatkivam@gmail.com>
2025-08-01 13:33:28 +0300

    fixed ft_Atoll function

--- utils/ft_atoll.c
+++ utils/ft_atoll.c
@@ -18,7 +18,7 @@ long long ft_atoll(const char* str)
     }
     while (str[i] >= '0' && str[i] <= '9')
     {
-        res = (res * 10) + (str[i] + '0');
+        res = (res * 10) + (str[i] - '0');
         i++;
     }
     return (res * sign);
