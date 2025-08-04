dfc7754377be3e84f75684b7dd3a126afae6c13b
suat kıvam <suatkivam@gmail.com>
2025-08-04 21:53:03 +0300

    is_overflow updated

--- error/is_overflow.c
+++ error/is_overflow.c
@@ -1,13 +1,31 @@
 #include "error.h"
 #include "../utils/utils.h"
 
-int is_overflow(const char* str)
+int is_overflow(const char *str)
 {
-    long long num;
+    int i = 0;
+    int sign = 1;
+    const char *limit = MAX_INT_STR;
+    const char *limit_neg = MIN_INT_STR;
 
-    num = ft_atoll(str);
+    if (str[i] == '+' || str[i] == '-')
+    {
+        if (str[i] == '-')
+            sign = -1;
+        i++;
+    }
 
-    if(num > MAX_INT || num < MIN_INT)
-        return (1);
-    return (0);
+    int len = ft_strlen(str + i);
+
+    if (len > 10)
+        return 1;
+    if (len < 10)
+        return 0;
+
+    if (sign == 1 && ft_strncmp(str + i, limit, 10) > 0)
+        return 1;
+    if (sign == -1 && ft_strncmp(str + i, limit_neg, 10) > 0)
+        return 1;
+
+    return 0;
 }
