a4a16608ac792c2f8988d5719aa310a6d556be2b
suat kıvam <suatkivam@gmail.com>
2025-08-04 21:53:27 +0300

    Validate_arguments updated

--- error/validate_arguments.c
+++ error/validate_arguments.c
@@ -1,31 +1,35 @@
+#include "../utils/utils.h"
 #include "error.h"
-#include "../utils/utils.h" 
 
-void validate_arguments(int argc, char **argv)
+void	validate_arguments(int argc, char **argv)
 {
-    int i = 1;
-    int *temp_numbers;
-    int count = argc - 1;
+	int			i;
+	int			*temp_numbers;
+	int			count;
+	long long	tmp;
 
-    while (i < argc)
-    {
-        if (is_not_numeric(argv[i]) || is_overflow(argv[i]))
-            exit_error();
-        i++;
-    }
-    temp_numbers = malloc(sizeof(int) * count);
-    if (!temp_numbers)
-        exit_error();
-    i = 0;
-    while (i < count)
-    {
-        temp_numbers[i] = ft_atoi(argv[i + 1]);
-        i++;
-    }
-    if (has_duplicates(temp_numbers, count))
-    {
-        free(temp_numbers);
-        exit_error();
-    }
-    free(temp_numbers);
+	i = 1;
+	count = argc - 1;
+	while (i < argc)
+	{
+		if (is_not_numeric(argv[i]) || is_overflow(argv[i]))
+			exit_error();
+		i++;
+	}
+	temp_numbers = malloc(sizeof(int) * count);
+	if (!temp_numbers)
+		exit_error();
+	i = 0;
+	while (i < count)
+	{
+		tmp = ft_atoll(argv[i + 1]);
+		temp_numbers[i] = (int)tmp;
+		i++;
+	}
+	if (has_duplicates(temp_numbers, count))
+	{
+		free(temp_numbers);
+		exit_error();
+	}
+	free(temp_numbers);
 }
