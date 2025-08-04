705e525202f7d055ea7a043de7f62848eec093f1
suat kıvam <suatkivam@gmail.com>
2025-08-04 21:52:38 +0300

    error.h updated

--- error/error.h
+++ error/error.h
@@ -3,6 +3,8 @@
 
 # define MIN_INT -2147483648
 # define MAX_INT 2147483647
+# define MIN_INT_STR "2147483648"
+# define MAX_INT_STR "2147483647"
 
 void	exit_error(void);
 int		is_not_numeric(const char *str);
