b17ff65094d92b2ff70a4c5052140affb5bccb7f
suat kıvam <suatkivam@gmail.com>
2025-09-09 11:47:05 +0300

    algortima kısmına başlandı

---
+++ algorithms/bubble_sort.c
@@ -0,0 +1,22 @@
+#include <utils.h>
+
+void bubble_sort(int arr*,int len)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (i < len -1)
+    {
+        while (j < len - 1 - i)
+        {
+            if(arr[j] > arr[j+1])
+            {
+
+            }
+            j++;
+        }
+        i++;
+    }
+    
+}
