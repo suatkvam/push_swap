afcd724c4dc2c9aee301c2c3fd4b92229958a982
suat kıvam <suatkivam@gmail.com>
2025-09-10 11:43:16 +0300

    utils güncellendi

--- utils/utils.h
+++ utils/utils.h
@@ -15,6 +15,7 @@ typedef struct s_stack
 
 long long	ft_atoll(const char *str);
 void		free_split_args(char **args);
+int			find_position_of_max(t_stack *stack);
 void		init_stack(t_stack *s_data, int capacity);
 void		push(t_stack *s_data, int value);
 int			pop(t_stack *s_data);
