2054d241808c3cfa83944fa1d52e3eaf1e56b903
suat kıvam <suatkivam@gmail.com>
2025-09-18 21:47:47 +0300

    algoritma dosyaları güncellendi

--- algorithms/algorithms.h
+++ algorithms/algorithms.h
@@ -1,11 +1,7 @@
 #ifndef ALGORITHMS_H
 # define ALGORITHMS_H
 # include "utils.h"
-typedef struct s_steps
-{
-	char			*step;
-	struct s_steps	*next;
-}					t_steps;
-# include "utils.h"
-// void				set_id_with_bubble_sort(t_stack *stack_a);
+
+typedef struct s_stack	t_stack;
+void					set_id_with_bubble_sort(t_stack *stack_a);
 #endif
--- algorithms/bubble_sort.c
+++ algorithms/bubble_sort.c
@@ -1,28 +1,28 @@
-// #include <utils.h>
+#include "utils.h"
+#include"algorithms.h"
+//elemanları boyutuna göre id ata
+void	set_id_with_bubble_sort(t_stack *stack_a)
+{
+	int i;
+	int j;
+	int len;
+	int idx;
 
-// //elemanları boyutuna göre id ata
-// void	set_id_with_bubble_sort(t_stack *stack_a)
-// {
-// 	int i;
-// 	int j;
-// 	int len;
-// 	int idx;
-
-// 	i = 0;
-// 	len = stack_a->top+1;
-// 	while (i < len)
-// 	{
-// 		j = 0;
-// 		idx = 0;
-// 		while (j < len)
-// 		{
-// 			if (stack_a->data[i] > stack_a->data[j])
-// 			{
-// 				idx++;
-// 			}
-// 			j++;
-// 		}
-// 		stack_a->id[i]= idx;
-// 		i++;
-// 	}
-// }
+	i = 0;
+	len = stack_a->top+1;
+	while (i < len)
+	{
+		j = 0;
+		idx = 0;
+		while (j < len)
+		{
+			if (stack_a->data[i] > stack_a->data[j])
+			{
+				idx++;
+			}
+			j++;
+		}
+		stack_a->id[i]= idx;
+		i++;
+	}
+}
