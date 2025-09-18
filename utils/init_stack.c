3af6c67db3ad9473dd057bbaaae5521682fcf5d4
suat kıvam <suatkivam@gmail.com>
2025-09-18 21:48:12 +0300

    init_stack id yer ayırma eklendi

--- utils/init_stack.c
+++ utils/init_stack.c
@@ -3,7 +3,8 @@
 void	init_stack(t_stack *s_data, int capacity)
 {
 	s_data->data = (int *)ft_calloc(capacity, sizeof(int));
-	if (!s_data->data)
+	s_data->id = (int *)ft_calloc(capacity,sizeof(int));
+	if (!s_data->data || !s_data->id)
 		exit_error();
 	s_data->top = -1;
 	s_data->capacity = capacity;
