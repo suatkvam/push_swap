07d81b5ef14c4ae378e1b601599e5a2b99b299b5
suat kıvam <suatkivam@gmail.com>
2025-09-19 22:13:16 +0300

    Fix: Update init_stack for proper capacity handling

--- utils/init_stack.c
+++ utils/init_stack.c
@@ -3,8 +3,7 @@
 void	init_stack(t_stack *s_data, int capacity)
 {
 	s_data->data = (int *)ft_calloc(capacity, sizeof(int));
-	s_data->id = (int *)ft_calloc(capacity,sizeof(int));
-	if (!s_data->data || !s_data->id)
+	if (!s_data->data)
 		exit_error();
 	s_data->top = -1;
 	s_data->capacity = capacity;
