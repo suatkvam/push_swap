cde19e4a703ddb9b9e757d519f13263b48a1dd58
suat kıvam <suatkivam@gmail.com>
2025-09-19 22:11:37 +0300

    Refactor: Split fill_stack_a into single-arg and multi-arg handlers

--- main.c
+++ main.c
@@ -3,74 +3,65 @@
 #include "utils.h"
 #include <stdio.h>
 
-/*init fill_stack a*/
-// todo: bu fonksiyon eklenip düzeltilecek
-/* Fill stack a with numbers from argv */
-static void	fill_stack_a(t_stack *stack_a, char **argv, int argc)
+/* argv tek string olarak verildiyse (argc == 2) */
+static void	fill_stack_from_single_arg(t_stack *stack_a, t_id_list *id_list_a,
+		char *arg)
 {
 	int		i;
 	int		split_data_len;
 	char	**split_data;
 
+	split_data = ft_split(arg, ' ');
+	if (!split_data)
+		exit(EXIT_FAILURE);
 	split_data_len = 0;
-	if (argc == 2)
-	{
-		split_data = ft_split(argv[1], ' ');
-		if (!split_data)
-			return ;
-		while (split_data[split_data_len])
-			split_data_len++;
-		init_stack(stack_a, split_data_len);
-		i = 0;
-		while (split_data[i])
-			push(stack_a, (int)ft_atoll(split_data[i++]));
-		free_split_args(split_data);
-	}
-	else
-	{
-		init_stack(stack_a, argc - 1);
-		i = 1;
-		while (i < argc)
-			push(stack_a, (int)ft_atoll(argv[i++]));
-	}
+	while (split_data[split_data_len])
+		split_data_len++;
+	init_stack(stack_a, split_data_len);
+	init_id_list(id_list_a, stack_a->capacity);
+	i = 0;
+	while (split_data[i])
+		push(stack_a, (int)ft_atoll(split_data[i++]));
+	free_split_args(split_data);
 }
 
-// ! bu fonksiyon silinecek
-void	print_stack(t_stack *s)
+/* argv çoklu argümanlarla geldiyse (argc > 2) */
+static void	fill_stack_from_args(t_stack *stack_a, t_id_list *id_list_a,
+		char **argv, int argc)
 {
 	int	i;
 
-	if (s->top < 0)
-	{
-		printf("stack bos\n");
-		return ;
-	}
-	i = s->top;
-	printf("stack(top -> bottom): ");
-	while (i >= 0)
-	{
-		printf("%d ", s->data[i]); // yukarıdan aşağı yazdırır
-		i--;
-	}
-	printf("\n");
+	init_stack(stack_a, argc - 1);
+	init_id_list(id_list_a, stack_a->capacity);
+	i = 1;
+	while (i < argc)
+		push(stack_a, (int)ft_atoll(argv[i++]));
 }
+
+/* Dispatcher */
+static void	fill_stack_a(t_stack *stack_a, t_id_list *id_list_a, char **argv,
+		int argc)
+{
+	if (argc == 2)
+		fill_stack_from_single_arg(stack_a, id_list_a, argv[1]);
+	else
+		fill_stack_from_args(stack_a, id_list_a, argv, argc);
+}
+
 /* Main function: entry point of the push_swap program */
 int	main(int argc, char **argv)
 {
-	t_stack	a;
-	t_stack	b;
-	char	**args;
-	int		count;
+	t_stack		a;
+	t_stack		b;
+	t_id_list	stack_id_list;
+	char		**args;
 
 	args = argv;
-	count = argc - 1;
-	init_stack(&b, argc - 1);
-	/* If no arguments provided, exit normally */
-	if (argc < 2)
-		exit(0);
 	check_arguments(argc, argv);
-	fill_stack_a(&a, args,argc);
-	set_id_with_bubble_sort(&a);
-	print_stack(&a);
+	fill_stack_a(&a, &stack_id_list, args, argc);
+	init_stack(&b, a.capacity);
+	set_rank_by_value(&a, &stack_id_list);
+	if (!is_sorted(&a, &stack_id_list))
+		printf("büyükten küçüğe doğru sirali");
 	return (0);
 }
