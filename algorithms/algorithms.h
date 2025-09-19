37db6b81f7477683b82e3635266eadf7edd8dc04
suat kıvam <suatkivam@gmail.com>
2025-09-19 22:13:31 +0300

    Update: Add function prototypes for set_rank_by_value and is_sorted

--- algorithms/algorithms.h
+++ algorithms/algorithms.h
@@ -2,6 +2,10 @@
 # define ALGORITHMS_H
 # include "utils.h"
 
-typedef struct s_stack	t_stack;
-void					set_id_with_bubble_sort(t_stack *stack_a);
+typedef struct s_stack t_stack;     // Forward declaration
+typedef struct s_id_list t_id_list; // Forward declaration
+/* ID assignment function */
+void	set_rank_by_value(t_stack *stack_a, t_id_list *id_list);
+int		is_sorted(t_stack *stack_data, t_id_list *id_list_data);
+
 #endif
