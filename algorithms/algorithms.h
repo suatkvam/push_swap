88c5a3276cf228c40e75473275917a8293bb1cf4
suat kıvam <suatkivam@gmail.com>
2025-09-21 22:33:04 +0300

    prototip eklendi

--- algorithms/algorithms.h
+++ algorithms/algorithms.h
@@ -9,5 +9,8 @@ void	assign_rank(t_stack *stack_a, t_id_list *id_list, int *sorted_arr)
 
 	int is_sorted(t_stack *stack_data, t_id_list *id_list_data);
 int		calculate_chunk_count(int list_size);
+int		*copy_stack_to_Arry(t_stack *stack_a);
+void	quick_sort(int *cp_arr, int left, int right);
+int		binary_search(int *cp_arr, int size, int value);
 
 #endif
