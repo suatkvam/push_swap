235cfd02c9a7ecd4d6d6e2543471fad1ff3abb10
suat kıvam <suatkivam@gmail.com>
2025-09-22 18:26:57 +0300

    \0

--- algorithms/algorithms.h
+++ algorithms/algorithms.h
@@ -5,7 +5,7 @@
 typedef struct s_stack t_stack;     // Forward declaration
 typedef struct s_id_list t_id_list; // Forward declaration
 /* ID assignment function */
-void	assign_rank(t_stack *stack_a, t_id_list *id_list, int *sorted_arr)
+void	assign_rank(t_stack *stack_a, t_id_list *id_list, int *sorted_arr);
 
 	int is_sorted(t_stack *stack_data, t_id_list *id_list_data);
 int		calculate_chunk_count(int list_size);
