8edfae7d61db2b88362cfbf7c18ba7a116b7b230
suat kıvam <suatkivam@gmail.com>
2025-09-23 19:24:08 +0300

    iki elemandaki kosul kontrolü düzeltildi

--- algorithms/sort.c
+++ algorithms/sort.c
@@ -1,22 +1,22 @@
 #include "algorithms.h"
 #include "utils.h"
 
+/*
+			Stack sağa doğru büyüdüğü için, 2 eleman olduğunda ilk eleman top
+				- 1,
+			ikinci eleman ise top indeksinde bulunuyor.”
+			sa(stack_a, id_list);
+		*/
 // !bu başka fonksiyonla birleşicek
 void	sort_five_and_under(t_stack *stack_a, t_id_list *id_list, int list_size)
 {
 	if (list_size == 2)
 	{
-		if (stack_a->data[stack_a->top] > stack_a->data[stack_a->top - 1])
+		if (stack_a->data[stack_a->top - 1] > stack_a->data[stack_a->top])
 			sa(stack_a, id_list);
 	}
-	// else if (list_size >= 3 && list_size <= 5)
-	// {
-	// 	while (!is_sorted(stack_a, id_list))
-	// 	{
-	// 		if (id_list->id[stack_a->top] > id_list->id[stack_a->top - 1])
-	// 			sa(stack_a, id_list);
-	// 		if (!is_sorted(stack_a, id_list))
-	// 			ra(stack_a, id_list);
-	// 	}
-	// }
+	else // Örnek 3 elemanlı minimal swap/rotate
+		if (list_size == 3)
+	{
+	}
 }
